e2581ab64992ed2f270ab66c345f34d65b948de9
Echa Aprilya <echaaprilya044@gmail.com>
2024-02-23 00:18:04 +0700

    pertemuan2_langkah praktikum4

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,7 +23,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Praktikum1
+//Praktikum2-3
 Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::get('/world', function () {
@@ -33,7 +34,6 @@ Route::get('/', [PageController::class, 'index']);
 
 Route::get('about', [PageController::class, 'about']);
 
-//Praktikum2
 Route::get('/user/{echa}', function ($echa) {
     return 'Nama saya '.$echa;
     });
@@ -45,7 +45,6 @@ Route::get('/posts/{post}/comments/{comment}', function
 
 Route::get('/articles/{id}', [PageController::class, 'articles']);
 
-//Praktikum3
 Route::get('/user/{name?}', function ($name=null) {
     return 'Nama saya '.$name;
 });
@@ -54,9 +53,17 @@ Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
 });
 
-//Praktikum4
+//Controller
 Route::get('/', HomeController::class);
 
 Route::get('/about', AboutController::class);
 
 Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class);
+
+//View
+Route::get('/greeting', function () {
+    return view('blog.hello', ['name' => 'Echa']);
+});
+    
