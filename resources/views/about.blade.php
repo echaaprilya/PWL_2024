9ac7995badef31ca0ec010ba6541776dddc4042b
Echa Aprilya <echaaprilya044@gmail.com>
2024-02-22 14:57:21 +0700

    Pertemuan2_langkah praktikum1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PWL 2024</title>
+    </head>
+    <body>
+        <h2>NIM: 2141762041</h2>
+        <h3>NAMA: ECHA APRILYA ACHMADIAH PUTRI</h3>
+    </body>
+</html>
